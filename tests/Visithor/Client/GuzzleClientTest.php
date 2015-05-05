975ff1ec7f14e4e9aff42b877781cec0380bd486
mmoreram <marc.morera@befactory.es>
2015-05-05 12:47:57 +0200

    Updated minimum guzzle version and requirements
    * Guzzle matches 4.* and 5.*

---
+++ tests/Visithor/Client/GuzzleClientTest.php
@@ -0,0 +1,46 @@
+<?php
+
+/*
+ * This file is part of the Visithor package.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * Feel free to edit as you please, and have fun.
+ *
+ * @author Marc Morera <yuhu@mmoreram.com>
+ */
+
+namespace Mmoreram\tests\Visithor\Client;
+
+use PHPUnit_Framework_TestCase;
+
+use Visithor\Client\GuzzleClient;
+use Visithor\Model\Url;
+
+/**
+ * Class GuzzleClientTest
+ */
+class GuzzleClientTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * Test client
+     */
+    public function testClient()
+    {
+        $client = new GuzzleClient();
+        $client->buildClient();
+        $url = new Url(
+            'http://google.es',
+            [200],
+            []
+        );
+
+        $result = $client->getResponseHTTPCode($url);
+
+        $this->assertEquals(
+            200,
+            $result
+        );
+    }
+}
