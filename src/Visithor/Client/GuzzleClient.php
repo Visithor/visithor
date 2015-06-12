f212fabe3034afeba30d2ba9613801481529c0d4
Igor Timoshenkov <igort@crytek.com>
2015-06-12 10:51:43 +0200

    Troubleshoot hhvm

--- src/Visithor/Client/GuzzleClient.php
+++ src/Visithor/Client/GuzzleClient.php
@@ -35,6 +35,7 @@ class GuzzleClient implements ClientInterface
     /**
      * Build client
      * @link http://php.net/manual/de/function.curl-setopt.php
+     * @link https://github.com/facebook/hhvm/issues/3737
      * @return $this Self object
      */
     public function buildClient()
@@ -72,11 +73,6 @@ class GuzzleClient implements ClientInterface
         // but we might be expecting a 404 or 500
         catch (RequestException $e) {
             $result = $e->getResponse()->getStatusCode();
-        }
-        // ignore phpUnit error notice for curl on hhvm
-        // @link https://github.com/facebook/hhvm/issues/3737
-        catch (\PHPUnit_Framework_Error_Notice $e) {
-
         }
         // anything other
         catch (Exception $e) {
